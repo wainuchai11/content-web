a575cd87bcd3aa1a0fec8c486b1e0c6ae445654b
wainuchai <jack.winuchai@gmail.com>
2022-10-18 02:18:39 +0700

    fix: deploy config

--- nuxt.config.js
+++ nuxt.config.js
@@ -18,9 +18,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-    "~layouts/global.css",
-],
+  css: ["~layouts/global.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -40,6 +38,7 @@ export default {
     "@nuxtjs/axios",
   ],
 
+  target: "static",
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
@@ -67,4 +66,7 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
+  generate: {
+    fallback: true,
+  },
 };
